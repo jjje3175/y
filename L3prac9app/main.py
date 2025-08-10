cd068b971273c44373bbc8994ea3531e5eeb6642
jjje3175 <Jean2013jjj@gmail.com>
2025-08-10 17:00:49 +0900

    適当なアプリ

---
+++ L3prac9app/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.title("ユーザー情報入力")
+# session stateの初期化
+if 'user_name' not in st.session_state:
+    st.session_state.user_name = ""
+#ユーザー名の入力
+name = st.text_input ("あなたの名前を入力してください")
+if st.button("名前在保存"):
+    st. session_state. user_name = name
+    st.success("名前を保存しました！")
+st.write (f"現在保存されている名前：{st.session_state.user_name}")
