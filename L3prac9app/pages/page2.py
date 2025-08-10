ce28cf71365cdfdd37846d3445196237b4f4e23e
jjje3175 <Jean2013jjj@gmail.com>
2025-08-10 17:52:37 +0900

    またコマンドミスがあった

--- L3prac9app/pages/page2.py
+++ L3prac9app/pages/page2.py
@@ -9,8 +9,9 @@ if 'user_name' in st.session_state.user_name:
     st.write("メインページで入力された名前が正しく表示されています。")
     # 追加の表示
     st.balloons() #祝福のアニメーション
-if x <2:
+if x <=2:
     st. success (f"こんにちは、{st.session_state.user_name}さん！")
+    st.write("メインページで入力された名前が正しく表示されています。")
     for i in range(3):
         st.balloons()
         time.sleep(1)
